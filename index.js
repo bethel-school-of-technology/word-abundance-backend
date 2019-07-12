85c1ac3f616ada82f47870cccefabefa184cc4a3
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-12 17:26:31 -0500

    connected to database

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let app = express();
 let mongoose = require('mongoose');
 
 // Connect to DB
+mongoose.connect(
+    'mongodb+srv://teamadmin:Password12@cluster0-mkdit.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true }, 
+    () => console.log('connected to db')
+);
 
 // Import Routes
 let authRoute = require('./routes/auth');
@@ -10,4 +15,4 @@ let authRoute = require('./routes/auth');
 // Route Middlewares
 app.use('/api/user', authRoute);
 
-app.listen(3000 , () => console.log("Server Up and Running"))
+app.listen(3000, () => console.log("Server Up and Running"))
