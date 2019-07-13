37ce72fed1ae01649990d215478af683efe26842
Joel Fernandez <joelfernandez0306@gmail.com>
2019-07-12 18:32:28 -0700

    Start of marketplace API. Added orders and products.

---
+++ api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/', (req, res, next )=>{
+    res.status(200).json({
+        message: 'Handling GET request to /products'
+    });
+});
+
+router.post('/', (req, res, next )=>{
+    res.status(200).json({
+        message: 'Handling POST request to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next)=>{
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message:'You discoverd the special ID'
+        });
+    }else {
+        res.status(200).json({
+            message: ' You passaed an ID'
+        })
+    }
+});
+
+
+router.patch('/:productId', (req, res, next)=>{
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next)=>{
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
