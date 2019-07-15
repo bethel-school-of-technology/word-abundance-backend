66b0be5496ab38abaa00611c703f49d342730fef
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-15 11:17:20 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 let router = require('express').Router();
 let User = require('../models/user');
 let {signupValidation}  = require('../validation')
-let bcrypt = require('bcrypt')
+let bcrypt = require('bcryptjs')
 
 router.get('/', (req, res) => {
     res.send('We are on home');
@@ -18,15 +18,15 @@ router.post('/signup', async (req, res) => {
     if (emailExist) return res.status(400).send('Email Already Exists');
 
 // Hash Passwords
-    let salt = await bcrypt.gentSalt(10);
-    let hashPassword = await bcrypt.hash(req.body.password, salt);
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 // Create a New User
     let user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
 
     });
     try {
