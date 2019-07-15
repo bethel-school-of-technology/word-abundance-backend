1264381ec96860a94062e3e4b9190df22be7bf15
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-15 09:20:11 -0500

    Add Bcrypt and edit auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 let router = require('express').Router();
 let User = require('../models/user');
-let Joi = require('@hapi/joi');
+let {signupValidation}  = require('../validation')
+let bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
     res.send('We are on home');
@@ -9,9 +10,17 @@ router.get('/', (req, res) => {
 router.post('/signup', async (req, res) => {
 
 // LETS VALIDATE THE DATA BEFORE WE ADD A USER TO DB
-    let {error} = Joi.validate(req.body,schema);
+    let {error} = signupValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+// Checker if user is already in database
+    let emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.status(400).send('Email Already Exists');
+
+// Hash Passwords
+    let salt = await bcrypt.gentSalt(10);
+
+// Create a New User
     let user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
