c583e8eb9e2095f038a0fa746c86990887a53ef5
Benjamin Chacko <benjaminchacko@outlook.com>
2019-07-20 18:17:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return 'Hello world!';
+    return 'Yahoo!';
   },
 };
 
