720c720528a5a67ed193a90cad156981cb4b74f2
Joel Fernandez <joelfernandez0306@gmail.com>
2019-07-24 09:28:53 -0700

    trying to connect to mongo

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-// app_ben
-let dotenv = require('dotenv');
-let cors = require('cors')
-let errorhandler = require('errorhandler')
+
 
 mongoose.connect('mongodb+srv://joelfernandez:' + process.env.MONGO_ATLAS_PW + '@abundant-2iz3d.mongodb.net/test?retryWrites=true&w=majority',
   {
@@ -31,12 +28,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 var app = express();
 
-// app_ben
-app.get('/', (req, res) => {
-  res.send('We are on home');
-});
-// Enviroment Variables
-dotenv.config();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,8 +41,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(morgan('dev'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-// app_ben
-app.use(cors());
+
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -75,10 +67,7 @@ app.use('/orders', orderRoutes);
 app.use('/services', serviceRoutes);
 app.use('/serviceorders', serviceOrderRoutes);
 
-// app_ben
-app.use('/user/signup', require('./api/routes/signup'));
-app.use('/user/login', require('./api/routes/login'));
-app.use('/posts', require('./api/routes/posts'));
+
 
 
 
@@ -94,20 +83,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // app_ ben Error Handler
-if (process.env.NODE_ENV === 'development') {
-  // only use in development
-  app.use(errorhandler({ log: errorNotification }))
 
-}
-function errorNotification (err, str, req) {
-  var title = 'Error in ' + req.method + ' ' + req.url
- 
-  notifier.notify({
-    title: title,
-    message: str
-  })
-}
 
   // render the error page
   res.status(err.status || 500);
